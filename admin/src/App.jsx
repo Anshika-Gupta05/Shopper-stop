2bbdbfa572ef5183861904a6fe5ba88f5555558c
Anshika Gupta <anshika.gupta2021@vitstudent.ac.in>
2025-03-06 17:57:49 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,7 +4,7 @@ import Admin from "./Pages/Admin/Admin";
 import Footer from "./Components/Footer/Footer";
 export const backend_url = "https://shopper-stop-backend.onrender.com";
 export const currency = "₹";
-export const frontend_url = "https://shopper-stop-frontend.onrender.com";
+export const frontend_url = "https://shopper-stop-frontend.vercel.app";
 
 const App = () => {
   return (
